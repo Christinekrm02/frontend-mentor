c25a00293fe733d98980dbfc41b6323c349046ce
Christine Maynard <christinekrm02@gmail.com>
2020-12-02 05:59:28 -0500

    working on left container

--- faq-accordion-card-main/main.js
+++ faq-accordion-card-main/main.js
@@ -1,7 +1,8 @@
 const accordion = document.getElementsByClassName("accordion");
 let i;
 
-/*iterate over each element in accordion and add/remove active class*/
+/*iterate over each element in accordion and add/remove active class to trigger hover states*/
+/* TODO: change onClick state to black from red*/
 for (i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function () {
     this.classList.toggle("active");
